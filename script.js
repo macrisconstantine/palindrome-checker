385461d92c59999e19300327a91761753ca3e5ef
Constantine Elias Macris <c.macris@acg.edu>
2025-07-14 20:41:19 -0500

    add code

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function checkClick() {
-  result.innerHTML = "hello world"
+  const txtInput = document.getElementById('text-input') 
+  if (txtInput.value == "") {
+    alert("please input a value");
+  }
+  if (txtInput.value.length == 1) {
+    result.innerHTML = txtInput.value + " is a palindrome."
+  }
 }
